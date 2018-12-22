4158bfb1d8383b753035631a511d291170681c63
rsidhu07 <rajsidhuh07@gmail.com>
2018-12-22 20:51:27 +0530

    Requirement form implemented

--- js/controller.js
+++ js/controller.js
@@ -1,53 +1,50 @@
 app.controller("myctrl", ($scope, myfactory) => {
 
     $scope.item = {};
-    //$scope.items = {};
+    $scope.items = {};
     $scope.selectedBloodGroup = "";
-    $scope.requiredBloodBottles = 0;
+    $scope.requiredBloodBottles = "";
     $scope.add = () => {
         //console.log("Add ",scope.item);
-        var items = myfactory.add($scope.item);
+        var items = myfactory.add($scope.item, $scope.bloodGroupToReceiverBloodsMap);
         $scope.items = items;
     };
 
     $scope.deleteAll = () => {
-        $scope.items = myfactory.deleteAll()
+        $scope.items = myfactory.deleteAll();
     }
 
-    // $scope.getBloodGroupToReceiverBloodsMap = () => {
-    let bloodGroupToReceiverBloodsMap = {}
-    bloodGroupToReceiverBloodsMap['A+'] = ['A+', 'A-', 'O+', 'O-']
-    bloodGroupToReceiverBloodsMap['O+'] = ['O+', 'O-']
-    bloodGroupToReceiverBloodsMap['B+'] = ['B+', 'B-', 'O+', 'O-']
-    bloodGroupToReceiverBloodsMap['AB+'] = ['A+', 'A-', 'O+', 'O-', 'B+', 'B-', 'AB+', 'AB-']
-    bloodGroupToReceiverBloodsMap['A-'] = ['A-', 'O-']
-    bloodGroupToReceiverBloodsMap['O-'] = ['O-']
-    bloodGroupToReceiverBloodsMap['B-'] = ['B-', 'O-']
-    bloodGroupToReceiverBloodsMap['AB-'] = ['B-', 'O-', 'AB-', 'A-']
+    let bloodGroupToReceiverBloodsMap = {};
+    bloodGroupToReceiverBloodsMap['A+'] = ['A+', 'A-', 'O+', 'O-'];
+    bloodGroupToReceiverBloodsMap['O+'] = ['O+', 'O-'];
+    bloodGroupToReceiverBloodsMap['B+'] = ['B+', 'B-', 'O+', 'O-'];
+    bloodGroupToReceiverBloodsMap['AB+'] = ['A+', 'A-', 'O+', 'O-', 'B+', 'B-', 'AB+', 'AB-'];
+    bloodGroupToReceiverBloodsMap['A-'] = ['A-', 'O-'];
+    bloodGroupToReceiverBloodsMap['O-'] = ['O-'];
+    bloodGroupToReceiverBloodsMap['B-'] = ['B-', 'O-'];
+    bloodGroupToReceiverBloodsMap['AB-'] = ['B-', 'O-', 'AB-', 'A-'];
     $scope.bloodGroupToReceiverBloodsMap = bloodGroupToReceiverBloodsMap;
-    //  return bloodGroupToReceiverBloodsMap
-    //}
 
-    // $scope.$watch("selectedBloodGroup", function (newValue, oldValue) {
     $scope.selectBloodGroup = () => {
-        //if (newValue && newValue != oldValue) {
-            let receiverBloodGroup = $scope.selectedBloodGroup;
-            let bloodGroupsCompatible = $scope.bloodGroupToReceiverBloodsMap[receiverBloodGroup]
-
-            //On change of selected blood group, set default color grey to all blood groups
-            for (let bloodGroup in $scope.items) {
-                $scope.items[bloodGroup].color = 'grey'
-            }
-            if (bloodGroupsCompatible) {
-                for (let bloodGroup of bloodGroupsCompatible) {
-                    if (!$scope.items[bloodGroup]) {
-                        continue;
-                    }
-                    $scope.items[bloodGroup].color = 'lightgreen'
+        let receiverBloodGroup = $scope.selectedBloodGroup;
+        let bloodGroupsCompatible = $scope.bloodGroupToReceiverBloodsMap[receiverBloodGroup];
+        let requiredBloodBottles = $scope.requiredBloodBottles;
+        //On change of selected blood group, set default color grey to all blood groups
+        for (let index in $scope.items) {
+            $scope.items[index].color = 'grey';
+        }
+        if (bloodGroupsCompatible) {
+            for (let bloodGroup of bloodGroupsCompatible) {
+                if (!$scope.items[bloodGroup]) {
+                    continue;
+                }
+                if (requiredBloodBottles && (requiredBloodBottles <= $scope.items[bloodGroup].bcount)) {
+                    $scope.items[bloodGroup].color = 'darkgreen';
+                    //$scope.items[bloodGroup].bcount -= requiredBloodBottles;
+                } else {
+                    $scope.items[bloodGroup].color = 'lightgreen';
                 }
             }
-        //}
+        }
     }
-    //})
-
 })
--- js/factory.js
+++ js/factory.js
@@ -2,17 +2,21 @@ app.factory("myfactory", () => {
 
     const itemOperations = {
         items: {},
-        add(itemObject) {
+        add(itemObject, bloodGroupToReceiverBloodsMap) {
             var item = new Item(itemObject.bname, itemObject.bcount);
             if (!item.bname) {
-                alert('Input blood group name to add number of bottles')
-                return
+                alert('Input blood group name to add number of bottles');
+                return;
             }
             if (!item.bcount) {
-                alert('Input number of bottles')
-                return
+                alert('Input number of bottles');
+                return;
             }
-            item.color = 'grey'
+            if (!bloodGroupToReceiverBloodsMap[item.bname]) {
+                alert('Please enter a valid blood group');
+                return;
+            }
+            item.color = 'grey';
             this.items[item.bname] = item;
             // this.items.push(itemObject); 
             return this.items;
