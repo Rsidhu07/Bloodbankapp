8aad1003b0939801271dd02bb9d1052c24d9de03
rsidhu07 <rajsidhuh07@gmail.com>
2018-12-22 19:43:42 +0530

    Bloodgroup selection implemented

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,53 @@
-app.controller("myctrl",($scope,myfactory)=>{
+app.controller("myctrl", ($scope, myfactory) => {
 
     $scope.item = {};
-    $scope.items = [];
-
-    $scope.add=()=>{
+    //$scope.items = {};
+    $scope.selectedBloodGroup = "";
+    $scope.requiredBloodBottles = 0;
+    $scope.add = () => {
         //console.log("Add ",scope.item);
         var items = myfactory.add($scope.item);
         $scope.items = items;
     };
 
+    $scope.deleteAll = () => {
+        $scope.items = myfactory.deleteAll()
+    }
 
+    // $scope.getBloodGroupToReceiverBloodsMap = () => {
+    let bloodGroupToReceiverBloodsMap = {}
+    bloodGroupToReceiverBloodsMap['A+'] = ['A+', 'A-', 'O+', 'O-']
+    bloodGroupToReceiverBloodsMap['O+'] = ['O+', 'O-']
+    bloodGroupToReceiverBloodsMap['B+'] = ['B+', 'B-', 'O+', 'O-']
+    bloodGroupToReceiverBloodsMap['AB+'] = ['A+', 'A-', 'O+', 'O-', 'B+', 'B-', 'AB+', 'AB-']
+    bloodGroupToReceiverBloodsMap['A-'] = ['A-', 'O-']
+    bloodGroupToReceiverBloodsMap['O-'] = ['O-']
+    bloodGroupToReceiverBloodsMap['B-'] = ['B-', 'O-']
+    bloodGroupToReceiverBloodsMap['AB-'] = ['B-', 'O-', 'AB-', 'A-']
+    $scope.bloodGroupToReceiverBloodsMap = bloodGroupToReceiverBloodsMap;
+    //  return bloodGroupToReceiverBloodsMap
+    //}
 
+    // $scope.$watch("selectedBloodGroup", function (newValue, oldValue) {
+    $scope.selectBloodGroup = () => {
+        //if (newValue && newValue != oldValue) {
+            let receiverBloodGroup = $scope.selectedBloodGroup;
+            let bloodGroupsCompatible = $scope.bloodGroupToReceiverBloodsMap[receiverBloodGroup]
 
+            //On change of selected blood group, set default color grey to all blood groups
+            for (let bloodGroup in $scope.items) {
+                $scope.items[bloodGroup].color = 'grey'
+            }
+            if (bloodGroupsCompatible) {
+                for (let bloodGroup of bloodGroupsCompatible) {
+                    if (!$scope.items[bloodGroup]) {
+                        continue;
+                    }
+                    $scope.items[bloodGroup].color = 'lightgreen'
+                }
+            }
+        //}
+    }
+    //})
 
 })
--- js/factory.js
+++ js/factory.js
@@ -1,17 +1,29 @@
-app.factory("myfactory",()=>{
+app.factory("myfactory", () => {
 
     const itemOperations = {
-        items:[],
-        add(itemObject){
+        items: {},
+        add(itemObject) {
             var item = new Item(itemObject.bname, itemObject.bcount);
-            this.items.push(item);
-          // this.items.push(itemObject); 
-           return this.items;
+            if (!item.bname) {
+                alert('Input blood group name to add number of bottles')
+                return
+            }
+            if (!item.bcount) {
+                alert('Input number of bottles')
+                return
+            }
+            item.color = 'grey'
+            this.items[item.bname] = item;
+            // this.items.push(itemObject); 
+            return this.items;
+        },
+        deleteAll() {
+            this.items = {}
+            return this.items
         }
-
     };
 
 
-        return itemOperations;
+    return itemOperations;
 
 })
